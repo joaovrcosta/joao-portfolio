9e8f0d6e5f89c41a826362f89b216385e0155c07
João Victor <joaovr.costa@gmail.com>
2024-06-03 12:46:53 -0300

    feat: added pages

---
+++ src/app/services/page.tsx
@@ -0,0 +1,5 @@
+const ServicesPage = () => {
+  return <div>page</div>
+}
+
+export default ServicesPage
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -5,19 +5,29 @@ import { usePathname } from 'next/navigation'
 
 const links = [
   { path: '/', name: 'Home' },
-  { path: '/about', name: 'services' },
+  { path: '/services', name: 'services' },
   { path: '/resume', name: 'resume' },
   { path: '/work', name: 'work' },
   { path: '/contact', name: 'contact' },
 ]
 
 function Nav() {
+  const pathName = usePathname()
+
   return (
-    <div>
-      {links.map((link, index) => {
-        return
-      })}
-    </div>
+    <nav className="flex gap-8">
+      {links.map((link, index) => (
+        <Link
+          key={index}
+          href={link.path}
+          className={`${
+            link.path === pathName && 'text-accent border-b-2 border-accent'
+          } capitalize font-medium hover:text-accent transition-all`}
+        >
+          {link.name}
+        </Link>
+      ))}
+    </nav>
   )
 }
 
